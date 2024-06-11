ba7ca8f27b6d08764bb4ac1d053376dda50011d1
Nelia-Chaban <126400490+Nelia-Chaban@users.noreply.github.com>
2024-06-11 13:12:39 +0300

    first commit

---
+++ src/js/02-timer.js
@@ -0,0 +1,21 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const startBtn = document.querySelector('button[data-start]');
+
+const days = document.querySelector('.value[data-days]');
+const hours = document.querySelector('.value[data-hours]');
+const minutes = document.querySelector('.value[data-minutes]');
+const seconds = document.querySelector('.value[data-seconds]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options);
